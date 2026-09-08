f58753f115ff0882f29add56928e62d3eb483c12
catalin200814 <corneliusalcoci14@gmail.com>
2026-09-08 09:59:23 +0300

    Am modificat codul js

--- laboratorul 1/lab1.js
+++ laboratorul 1/lab1.js
@@ -1,46 +1,21 @@
-let seconds = 0;
-let minutes = 0;
-let intervalId = null;
-let isRunning = false;
+let s=0,m=0,i=null;
 
-const timerDisplay = document.getElementById('timerDisplay');
-const startBtn = document.getElementById('startBtn');
-const pauseBtn = document.getElementById('pauseBtn');
-const resetBtn = document.getElementById('resetBtn');
-
-function updateDisplay() {
-  let min = minutes < 10 ? '0' + minutes : minutes;
-  let sec = seconds < 10 ? '0' + seconds : seconds;
-  timerDisplay.textContent = min + ':' + sec;
+function cronometru(actiune) {
+  if(actiune=='start' && i==null) {
+    i=setInterval(()=>{
+      s++;
+      if(s==60){s=0;m++}
+      document.getElementById('timer').innerHTML=(m<10?'0':'')+m+':'+(s<10?'0':'')+s
+    },1000)
+  }
+  if(actiune=='stop' && i!=null) {
+    clearInterval(i)
+    i=null
+  }
+  if(actiune=='reset') {
+    clearInterval(i)
+    i=null
+    s=0;m=0
+    document.getElementById('timer').innerHTML='00:00'
+  }
 }
-
-function startTimer() {
-  if (isRunning) return;
-  isRunning = true;
-  intervalId = setInterval(function() {
-    seconds++;
-    if (seconds === 60) {
-      seconds = 0;
-      minutes++;
-    }
-    updateDisplay();
-  }, 1000);
-}
-
-function pauseTimer() {
-  if (!isRunning) return;
-  isRunning = false;
-  clearInterval(intervalId);
-}
-
-function resetTimer() {
-  clearInterval(intervalId);
-  isRunning = false;
-  seconds = 0;
-  minutes = 0;
-  updateDisplay();
-}
-
-startBtn.addEventListener('click', startTimer);
-pauseBtn.addEventListener('click', pauseTimer);
-resetBtn.addEventListener('click', resetTimer);
